54e13a3d5eb5d5a1a90f11b2c2141388114e2c5f
Rifan09 <muhammadrifani0923@gmail.com>
2025-09-04 22:51:15 +0800

    menambahkan file pada folder pkbm untuk rute dan juga pengaturan setting

--- pkbm/settings.py
+++ pkbm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pkbmapp',
+    'django_adminlte3',
+    # 'django_adminlte3.admin'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'pkbm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR /'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- pkbm/urls.py
+++ pkbm/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pkbmapp import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+   # profiles sebagai root
+    path('', include('pkbmapp.urls')),
+
+#     # UI Publik
+#     path('sekolah/', views.sekolah_detail, name="sekolah_detail"),
+#     path('sekolah/edit/', views.sekolah_update, name="sekolah_update"),
+
+#     path('program/', views.program_list, name="program_list"),
+#     path('program/add/', views.program_create, name="program_create"),
+#     path('program/<int:pk>/edit/', views.program_update, name="program_update"),
+#     path('program/<int:pk>/delete/', views.program_delete, name="program_delete"),
 ]
