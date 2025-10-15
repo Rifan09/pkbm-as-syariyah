7431612241012390cfc2de7d459c5fd546c3fc10
Rifan09 <muhammadrifani0923@gmail.com>
2025-10-15 21:49:50 +0800

    menyesuaikan route dari berita

--- pkbmapp/views.py
+++ pkbmapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Galeri
+from django.shortcuts import render, get_object_or_404
+from .models import Galeri, Berita
+from django.core.paginator import Paginator
 
 
 
@@ -12,12 +13,25 @@ def profil(request):
 def program(request):
     return render(request, 'pkbmapp/program.html')
 
-def berita(request):
-    return render(request, 'pkbmapp/berita.html')
-
 def kontak(request):
     return render(request, 'pkbmapp/kontak.html')
 
+
+def berita(request):
+    berita_all = Berita.objects.all()
+    paginator = Paginator(berita_all, 6)  # 6 berita per halaman
+    page_number = request.GET.get('page')
+    berita_page = paginator.get_page(page_number)
+    
+    featured = berita_all.first()
+    return render(request, 'pkbmapp/berita.html', {
+        'featured': featured,
+        'berita_page': berita_page,
+    })
+
+def berita_detail(request, slug):
+    berita = get_object_or_404(Berita, slug=slug)
+    return render(request, 'pkbmapp/berita_detail.html', {'berita': berita})
 
 def galeri(request):
     galeri_per_bulan = {}
