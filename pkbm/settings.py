878df921685001a84ceb47afb58912f34ed404ad
Rifan09 <muhammadrifani0923@gmail.com>
2025-10-23 23:36:30 +0800

    update settings

--- pkbm/settings.py
+++ pkbm/settings.py
@@ -65,9 +65,9 @@ DATABASES = {
 
 # ================= CLOUDINARY ==================
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('drptjbbky'),
+    'API_KEY': os.environ.get('428159951645772'),
+    'API_SECRET': os.environ.get('fIQwxPTId0ZK-M_SjNOEeJ82lTs'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
